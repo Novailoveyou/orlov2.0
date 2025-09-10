01d82c496e40526af1fb64117f9318ac9fdaf3ff
novailoveyou <novailoveyoux@gmail.com>
2025-09-10 23:47:26 +0300

    fix: ym

--- apps/web/entities/yandex-metrika.ts
+++ apps/web/entities/yandex-metrika.ts
@@ -3,7 +3,7 @@ import 'client-only'
 
 import { createYandexMetrika } from '@repo/next-yandex-metrika'
 
-export const { ym, YandexMetrika } = createYandexMetrika({
+export const { ym, YandexMetrika } = createYandexMetrika<'lead'>({
   yaCounterId: 0,
   visitParams: {
     currency: 'RUB',
