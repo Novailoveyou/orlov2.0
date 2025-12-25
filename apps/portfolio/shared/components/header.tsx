d2aa7de16f8ce483d87ee3dfd1c7113a23e7b8a9
novaloveyoux <novailoveyoux@gmail.com>
2025-12-25 18:58:30 +0300

    feat: lang toggle

--- apps/portfolio/shared/components/header.tsx
+++ apps/portfolio/shared/components/header.tsx
@@ -3,18 +3,17 @@ import 'server-only'
 import { Header as HeaderBase } from '@repo/ui/components/tags/header'
 import { ModeToggle } from '@/shared/components/mode-toggle'
 import { Logo } from '@/shared/images/logo'
-import { Link } from '@/shared/components/link'
+import { LangToggle } from './lang-toggle'
+import { Suspense } from 'react'
 
-export const Header = () => (
-  <HeaderBase className='flex flex-wrap justify-center items-center gap-4 mb-6'>
-    {/* lang toggle here */}
-    <Link href='/' locale='ru' pending='Loading...'>
-      ru
-    </Link>
-    <Link href='/' locale='en' pending='Loading...'>
-      en
-    </Link>
-    {/* <Logo className='ml-auto' /> */}
-    <ModeToggle className='ml-auto' />
-  </HeaderBase>
-)
+export const Header = () => {
+  return (
+    <HeaderBase className='flex flex-wrap justify-center items-center gap-4 mb-6'>
+      <Suspense>
+        <LangToggle />
+      </Suspense>
+      {/* <Logo className='ml-auto' /> */}
+      <ModeToggle className='ml-auto' />
+    </HeaderBase>
+  )
+}
