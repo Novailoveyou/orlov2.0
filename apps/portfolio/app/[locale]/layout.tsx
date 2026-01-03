2d9c9679c6b3e698530db17eae6807f72db448b1
novailoveyou <novailoveyoux@gmail.com>
2026-01-03 22:35:07 +0300

    fix: layout

--- apps/portfolio/app/[locale]/layout.tsx
+++ apps/portfolio/app/[locale]/layout.tsx
@@ -76,12 +76,10 @@ export default async function RootLayout({
                   <WebVitals />
                   <PullToRefresh>
                     <Header />
-                    <Main>
-                      {children}
-                      {dialog}
-                    </Main>
+                    <Main>{children}</Main>
                     <Footer />
                   </PullToRefresh>
+                  {dialog}
                   <NavigationMenu />
                 </Container>
                 <Toaster />
