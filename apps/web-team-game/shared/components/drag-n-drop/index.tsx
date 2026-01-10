2921a937a56bdc21a9533c077b45d1c56c2b5b97
novailoveyou <novailoveyoux@gmail.com>
2026-01-10 21:40:31 +0300

    fix: mobile touch

--- apps/web-team-game/shared/components/drag-n-drop/index.tsx
+++ apps/web-team-game/shared/components/drag-n-drop/index.tsx
@@ -3,10 +3,11 @@ import 'client-only'
 
 import { ComponentProps } from 'react'
 import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
+// import { HTML5Backend } from 'react-dnd-html5-backend'
+import { TouchBackend } from 'react-dnd-touch-backend'
 
 export const DragNDropProvder = ({
   children,
 }: Pick<ComponentProps<typeof DndProvider>, 'children'>) => {
-  return <DndProvider backend={HTML5Backend}>{children}</DndProvider>
+  return <DndProvider backend={TouchBackend}>{children}</DndProvider>
 }
