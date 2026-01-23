af0dd7678e18396184a518afd2a05bc05c2c86b4
novailoveyou <novailoveyoux@gmail.com>
2026-01-23 14:05:15 +0300

    fix: apple cion

--- apps/portfolio/features/logo/logo-image-static.tsx
+++ apps/portfolio/features/logo/logo-image-static.tsx
@@ -1,12 +1,21 @@
 import 'server-only'
 
 import { Image } from '@/shared/components/image'
-import logo from './logo.svg'
+// import logo from './logo.svg'
 import { ComponentProps } from 'react'
 import { APP_NAME } from '@/shared/constants'
 
 export const LogoImageStatic = ({
   alt = APP_NAME,
 }: Partial<Pick<ComponentProps<typeof Image>, 'alt'>>) => {
-  return <Image src={logo} desiredWidth={38} alt={alt} priority />
+  return (
+    <Image
+      src={'/vercel.svg'}
+      width={38}
+      height={38}
+      desiredWidth={38}
+      alt={alt}
+      priority
+    />
+  )
 }
